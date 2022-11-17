b8179819a74b4a2f0c5a127ca09fdac06995ca8f
Rowan Dash <pokemonmaster9967@gmail.com>
2022-11-17 11:29:12 +0000

    Update FlyingAssassin.java

--- src/main/java/com/example/cs230assignment/FlyingAssassin.java
+++ src/main/java/com/example/cs230assignment/FlyingAssassin.java
@@ -3,7 +3,7 @@ package com.example.cs230assignment;
 public class FlyingAssassin extends NPC {
 
     public void FlyingAssassin(coordChangePass){
-        this.coordChange = coordChangePass
+        this.coordChange = coordChangePass;
     }
 
     // was flying assassin kill changed to fit better with boardupdate
