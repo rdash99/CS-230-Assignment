5d35c52aa18b4f0fd164dd21cbe778988d4dd8df
Rowan Dash <pokemonmaster9967@gmail.com>
2022-11-18 14:24:45 +0000

    Menu skeleton

---
+++ src/main/java/com/example/cs230assignment/Menu.java
@@ -0,0 +1,11 @@
+package com.example.cs230assignment;
+
+public class Menu extends DrawShape {
+    public Menu() {
+        super();
+    }
+
+    public void draw() {
+        System.out.println("Menu");
+    }
+}
