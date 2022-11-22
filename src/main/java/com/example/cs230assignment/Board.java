c4eafae5057ee02fe3ff0f85dadc9387eed1837d
tjmcauley <105383529+tjmcauley@users.noreply.github.com>
2022-11-22 17:02:19 +0000

    Given Board methods and attributes.
    Board has all methods and attributes as specified in the hierarchy diagram.

--- src/main/java/com/example/cs230assignment/Board.java
+++ src/main/java/com/example/cs230assignment/Board.java
@@ -1,5 +1,25 @@
 package com.example.cs230assignment;
 
-public class Board {
+import javafx.scene.canvas.GraphicsContext;
 
+public class Board extends DrawShape{
+    private Tile[][] tiles;
+    private int[][] dimensions;
+    private Player player;
+
+    public Tile getTile(int x, int y) {
+        return this.tiles[x][y];
+    }
+
+    public int[][] getDimensions() {
+        return this.dimensions;
+    }
+
+    public Player getPlayer() {
+        return this.player;
+    }
+
+    @Override
+    public void draw() {
+    }
 }
