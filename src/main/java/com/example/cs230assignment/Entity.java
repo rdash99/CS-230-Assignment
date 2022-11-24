28b344eec74e00633a620f845361e173e6a57115
Rowan Dash <pokemonmaster9967@gmail.com>
2022-11-24 13:08:42 +0000

    Update Entity.java

--- src/main/java/com/example/cs230assignment/Entity.java
+++ src/main/java/com/example/cs230assignment/Entity.java
@@ -1,16 +1,27 @@
 package com.example.cs230assignment;
 
+/**
+ * Defines the Entity class. This class is the parent class for several other
+ * classes.
+ * 
+ * @author Rowan Dash
+ * @version 1.0
+ */
 public class Entity extends DrawShape {
     private String entityName;
     protected int[] coord;
 
-    public void Entity(String entityNamePass, int[] coordPass) {
+    public Entity(String entityNamePass, int[] coordPass) {
         this.coord = coordPass;
         this.entityName = entityNamePass;
     }
 
     public String getEntityName() {
         return this.entityName;
+    }
+
+    public int[] getCoord() {
+        return this.coord;
     }
 
     @Override
