6600459fdf7d6a6da5c67605ea1143e817b60bb7
charlesOverton <74202093+CharlesOverton@users.noreply.github.com>
2022-11-28 13:27:04 +0000

    moved board to character and interact update
    title says all

--- src/main/java/com/example/cs230assignment/Board.java
+++ src/main/java/com/example/cs230assignment/Board.java
@@ -1,5 +1,10 @@
 package com.example.cs230assignment;
 
 public class Board {
+    private Tile[][] tiles;
 
+
+    public getTile(int[] coordPass){
+        return this.tiles[coordPass[0]][coordPass[1]];
+    }
 }
--- src/main/java/com/example/cs230assignment/Character.java
+++ src/main/java/com/example/cs230assignment/Character.java
@@ -1,9 +1,17 @@
 package com.example.cs230assignment;
 
 public class Character extends Entity {
+<<<<<<< Updated upstream
     public void Character() {
 
+=======
+    //moved up from NPC as the player will also need board
+    protected Board currentBoard;
+    public Character() {
+        super(null, null);
+>>>>>>> Stashed changes
     }
+
 
     protected void move() {
 
@@ -13,8 +21,18 @@ public class Character extends Entity {
 
     }
 
-    private void interact(Item interactedItem) {
-
+    private void interact() {
+        for( int i = 0; i <board.getTile(coord).getEntities().length(); i++){
+            if (board.getTile(coord).getEntities()[i].getEntityName() == "Item"){
+                board.getTile(coord).removeEntity(board.getTile(coord).getEntities()[i]);
+            }elif (board.getTile(coord).getEntities()[i].getEntityName() == "leaver"){
+                board.getTile(coord).getEntities()[i].openGate();
+            }elif (board.getTile(coord).getEntities()[i].getEntityName() == "clock"){
+                time.addClock();
+            }elif (board.getTile(coord).getEntities()[i].getEntityName() == "player"){
+                board.getPlayer().die()
+            }
+        }
     }
     // protected void drawMove(){
     // }
