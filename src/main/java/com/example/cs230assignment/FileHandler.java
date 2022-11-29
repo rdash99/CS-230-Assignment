8e3d5a6482c727e27ba20f682e5fe63fe34550a7
Rowan Dash <pokemonmaster9967@gmail.com>
2022-11-29 11:05:49 +0000

    Handle reading level file tiles and player

--- src/main/java/com/example/cs230assignment/FileHandler.java
+++ src/main/java/com/example/cs230assignment/FileHandler.java
@@ -2,6 +2,7 @@ package com.example.cs230assignment;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -21,6 +22,45 @@ public class FileHandler {
             System.out.println("Could not find" + fileName);
             System.exit(0);
         }
+        int x;
+        int y;
+        String sizeLine = in.nextLine();
+        String[] size = sizeLine.split(" ");
+        Tile[][] tiles = new Tile[Integer.parseInt(size[0])][Integer
+                .parseInt(size[1])];
+        ArrayList<Entity> entities = new ArrayList<Entity>();
+        Player player = null;
+        x = Integer.parseInt(size[0]);
+        y = Integer.parseInt(size[1]);
+
+        int yNum = y * (-1);
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            String[] lineArray = line.split(" ");
+            int xNum = x * (-1);
+            yNum = yNum + 1;
+            for (int i = 0; i < lineArray.length; i++) {
+                if (lineArray[i].length() == 4) {
+                    tiles[xNum][yNum] = new Tile(lineArray[i].charAt(0),
+                            lineArray[i].charAt(1), lineArray[i].charAt(2),
+                            lineArray[i].charAt(3));
+                }
+                ;
+                if (lineArray[i].length() == 3) {
+                    String[] coords = lineArray[i - 1].split(",");
+                    int xCoord = Integer.parseInt(coords[0]);
+                    int yCoord = Integer.parseInt(coords[1]);
+                    if (lineArray[i].equals("PLR")) {
+                        Player player1 = new Player(xCoord, yCoord);
+                        player = player1;
+                        break;
+                    }
+                }
+                ;
+                xNum = xNum + 1;
+            }
+        }
+        in.close();
         return null;
     }
 
