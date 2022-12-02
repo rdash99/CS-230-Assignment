e7c37c7df86eafd2ab469f71baf9b1aca56aece9
Rowan Dash <pokemonmaster9967@gmail.com>
2022-12-02 16:56:13 +0000

    Merging conflicts

--- src/main/java/com/example/cs230assignment/Character.java
+++ src/main/java/com/example/cs230assignment/Character.java
@@ -1,18 +1,15 @@
 package com.example.cs230assignment;
 
 public class Character extends Entity {
-    <<<<<<<
-    Updated upstream
-
     public Character(String name, int x, int y) {
         super(name, x, y);
-=======
-    //moved up from NPC as the player will also need board
+    }
+
+    // moved up from NPC as the player will also need board
     protected Board currentBoard;
 
     public Character() {
         super(null, null);
->>>>>>> Stashed changes
     }
 
     protected void move() {
@@ -24,15 +21,21 @@ public class Character extends Entity {
     }
 
     private void interact() {
-        for( int i = 0; i <board.getTile(coord).getEntities().length(); i++){
-            if (board.getTile(coord).getEntities()[i].getEntityName() == "Item"){
+        for (int i = 0; i < board.getTile(coord).getEntities().length(); i++) {
+            if (board.getTile(coord).getEntities()[i].getEntityName() == "Item") {
                 board.getTile(coord).removeEntity(board.getTile(coord).getEntities()[i]);
-            }elif (board.getTile(coord).getEntities()[i].getEntityName() == "leaver"){
+            }
+            elif(board.getTile(coord).getEntities()[i].getEntityName() == "leaver");
+            {
                 board.getTile(coord).getEntities()[i].openGate();
-            }elif (board.getTile(coord).getEntities()[i].getEntityName() == "clock"){
+            }
+            elif(board.getTile(coord).getEntities()[i].getEntityName() == "clock");
+            {
                 time.addClock();
-            }elif (board.getTile(coord).getEntities()[i].getEntityName() == "player"){
-                board.getPlayer().die()
+            }
+            elif(board.getTile(coord).getEntities()[i].getEntityName() == "player");
+            {
+                board.getPlayer().die();
             }
         }
     }
