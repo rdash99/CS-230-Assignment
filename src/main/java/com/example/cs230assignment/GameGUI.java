9c14c036407b75c1d331d41e72f4ee321497195b
tjmcauley <105383529+tjmcauley@users.noreply.github.com>
2022-12-04 17:15:01 +0000

    readLevel file is static so removed instance of filehandler

--- src/main/java/com/example/cs230assignment/GameGUI.java
+++ src/main/java/com/example/cs230assignment/GameGUI.java
@@ -128,8 +128,7 @@ public class GameGUI extends Stage {
 //        Entity[] entities = new Entity[] {entity1};
 //        Player player = new Player(0, 0);
 //        Board board = new Board(5, 2, tiles, player, entities);
-        FileHandler filehandler = new FileHandler();
-        filehandler.readLevelFile("H:\\Year 2 Computer Science\\CS-230\\A2\\CS-230-Assignment\\src\\main\\resources\\testLevel").draw(gc);
+          FileHandler.readLevelFile("H:\\Year 2 Computer Science\\CS-230\\A2\\CS-230-Assignment\\src\\main\\resources\\testLevel").draw(gc);
 //
         // Get the Graphic Context of the canvas. This is what we draw on.
         gc = canvas.getGraphicsContext2D();
