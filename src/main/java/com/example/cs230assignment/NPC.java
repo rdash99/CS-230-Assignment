015c2aebc3423006502a67383280596a0d43cbb1
charlesOverton <74202093+CharlesOverton@users.noreply.github.com>
2022-12-04 17:04:14 +0000

    Update NPC.java

--- src/main/java/com/example/cs230assignment/NPC.java
+++ src/main/java/com/example/cs230assignment/NPC.java
@@ -27,11 +27,6 @@ public class NPC extends Character {
       this. coord[1] = this.coord[1] + this.coordChange[1]
     }
 
-    public void move() {
-        validMove(this.coord);
-        this.coord[0] = this.coord[0] + this.coordChange[0];
-        this.coord[1] = this.coord[1] + this.coordChange[1];
-    }
 
     protected double getMovementTimer() {
         return movementTimer;
