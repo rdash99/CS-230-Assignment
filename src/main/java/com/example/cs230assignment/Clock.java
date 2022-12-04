fcab88c0c4ff256a69430320108710f23e77e87b
belairemaxwell <professowell217@gmail.com>
2022-12-04 16:29:25 +0000

    Clock Update

--- src/main/java/com/example/cs230assignment/Clock.java
+++ src/main/java/com/example/cs230assignment/Clock.java
@@ -1,5 +1,11 @@
 package com.example.cs230assignment;
 
-public class Clock {
+public class Clock extends Entity {
+    private int clockTime = 0;
+
+    public Clock (String clockName, int addedTime, int xCoord, int yCoord) {
+        super(entityNamePass, x, y);
+        this.clockTime = addedTime;
+    }
 
 }
