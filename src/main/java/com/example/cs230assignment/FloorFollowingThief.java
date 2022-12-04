2f4904a451f770ceafa355884e427fcf76e00945
charlesOverton <74202093+CharlesOverton@users.noreply.github.com>
2022-12-04 17:48:08 +0000

    floor following syntax
    fgn

--- src/main/java/com/example/cs230assignment/FloorFollowingThief.java
+++ src/main/java/com/example/cs230assignment/FloorFollowingThief.java
@@ -13,7 +13,7 @@ class FloorFollowingThief extends NPC {
         // Allocated a timer of 0.5 seconds for the thief
         super("Floor Following Thief", 0.5, x, y);
         this.allocatedColour = allocatedColourPass;
-        this.coordChange = coordChangePass;
+        super.coordChange = {x,y};
     }
 
     private void interact() {
@@ -25,17 +25,17 @@ class FloorFollowingThief extends NPC {
     private void validMove() {
       //changes the coordChange to the coord to the left
       switch(this.coordChange){
-        case [1,0]:
-          this.coordChange = [0,1];
+        case {1,0}:
+          this.coordChange = {0,1};
           break;
-        case [0,1]:
-          this.coordChange = [-1,0];
+        case {0,1}:
+          this.coordChange = {-1,0};
           break;
-        case [-1,0]:
-          this.coordChange = [0,-1];
+        case {-1,0}:
+          this.coordChange = {0,-1};
           break;
-        case [0,-1]:
-          this.coordChange = [1,0];
+        case {0,-1}:
+          this.coordChange = {1,0};
           break;
       }
       //loops in all directions to check for a valid move
@@ -47,7 +47,7 @@ class FloorFollowingThief extends NPC {
                 i = 5;
                 break;
               }else{
-                this.coordChange = [0,-1];
+                this.coordChange = {0,-1};
                 break;
               }
             case [0,1]:
@@ -55,7 +55,7 @@ class FloorFollowingThief extends NPC {
                 i = 5;
                 break;
               }else{
-                this.coordChange = [1,0];
+                this.coordChange = {1,0};
                 break;
               }
             case [0,-1]:
@@ -63,7 +63,7 @@ class FloorFollowingThief extends NPC {
                 i = 5;
                 break;
               }else{
-                this.coordChange = [-1,0];
+                this.coordChange = {-1,0};
                 break;
               }
             case [-1,0];
@@ -71,12 +71,12 @@ class FloorFollowingThief extends NPC {
                 i = 5;
                 break;
               }else{
-                this.coordChange = [0,1];
+                this.coordChange = {0,1};
                 break;
               }
           }
         }else{
-          this.coordChange = [0,0];
+          this.coordChange = {0,0};
         }
       }
     }
