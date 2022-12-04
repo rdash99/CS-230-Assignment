ee85a4aa4223a0ffa29a7149ea74f97f8d41a955
Rowan Dash <pokemonmaster9967@gmail.com>
2022-12-04 16:41:53 +0000

    Update FileHandler.java

--- src/main/java/com/example/cs230assignment/FileHandler.java
+++ src/main/java/com/example/cs230assignment/FileHandler.java
@@ -120,7 +120,7 @@ public class FileHandler {
     }
 
     public static Board readLevelFile(String fileName) {
-        File file = new File(fileName + ".txt");
+        File file = new File("levels" + fileName + ".txt");
         return loadBoard(file);
     }
 
