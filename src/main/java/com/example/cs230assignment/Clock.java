5d77a39b92678f30a21855d7214c5dfd17ec4234
belairemaxwell <professowell217@gmail.com>
2022-12-04 16:55:17 +0000

    Clock Update

--- src/main/java/com/example/cs230assignment/Clock.java
+++ src/main/java/com/example/cs230assignment/Clock.java
@@ -3,9 +3,20 @@ package com.example.cs230assignment;
 public class Clock extends Entity {
     private int clockTime = 0;
 
-    public Clock (String clockName, int addedTime, int xCoord, int yCoord) {
+    public Clock (String entityNamePass, int addedTime, int x, int y) {
         super(entityNamePass, x, y);
         this.clockTime = addedTime;
     }
 
+    public void setClockTime (int additionalTime) {
+        this.clockTime = additionalTime;
+    }
+
+    public void getClockTime () {
+        return clockTime;
+    }
+    
+    public void addTime (int additionalTime) {
+
+    }
 }
