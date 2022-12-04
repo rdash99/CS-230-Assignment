fcc40e42f79f925b3411cb35e7bf9aa78c84c301
Frasukai <69215668+Frasukai@users.noreply.github.com>
2022-12-04 20:38:45 +0000

    partial motd implementation

--- src/main/java/com/example/cs230assignment/MainMenu.java
+++ src/main/java/com/example/cs230assignment/MainMenu.java
@@ -13,13 +13,10 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.concurrent.ExecutionException;
-import java.util.function.Supplier;
 
 public class MainMenu extends Stage {
 
@@ -28,23 +25,32 @@ public class MainMenu extends Stage {
     private Button launchGameBtn = new Button("Start the game");
     private Text titleText = new Text("Welcome to the game!");
     private Text scoresTitle = new Text("High scores: ");
+    private Text motdTitle = new Text("Message of the day: ");
     private VBox middleButtons = new VBox();
     private VBox highScoreList = new VBox();
-    private VBox mOTD = new VBox();
+    private VBox motd = new VBox();
     private HBox hbox = new HBox();
 
-    public MainMenu() {
+
+
+    public MainMenu() throws IOException, InterruptedException {
         titleText.setFont(Font.font ("Arial", FontWeight.BOLD, 20));
         titleText.setFill(Color.GRAY);
 
         scoresTitle.setFont(Font.font ("Arial",FontWeight.BOLD, 15));
         scoresTitle.setFill(Color.GRAY);
 
+        motdTitle.setFont(Font.font ("Arial",FontWeight.BOLD, 15));
+        motdTitle.setFill(Color.GRAY);
+
         middleButtons.getChildren().add(launchGameBtn);
         middleButtons.setAlignment(Pos.CENTER);
         middleButtons.setSpacing(20);
 
         highScoreList.getChildren().add(scoresTitle);
+
+        motd.getChildren().add(motdTitle);
+        motd.getChildren().add(new Text(getsolvedMOTD(solvePuzzle())));
 
         hbox.getChildren().add(titleText);
         hbox.setAlignment(Pos.CENTER);
@@ -55,7 +61,7 @@ public class MainMenu extends Stage {
         root.setTop(hbox);
         root.setCenter(middleButtons);
         root.setLeft(highScoreList);
-        root.setRight(mOTD);
+        root.setRight(motd);
 
         this.setScene(new Scene(root, 600, 500));
         this.setTitle("The game menu");
@@ -76,9 +82,33 @@ public class MainMenu extends Stage {
         highScoreList.getChildren().add(scoreText);
     }
 
-    private static void synchronousRequest() throws IOException, InterruptedException {
-        //here
+    private String getMOTDPuzzle() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://cswebcat.swansea.ac.uk/puzzle"))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
     }
+
+    private String solvePuzzle() throws IOException, InterruptedException {
+        String puzzle = getMOTDPuzzle();
+        String answer = "";
+
+        //TODO: implement this method
+        return answer;
+    }
+
+    private String getsolvedMOTD(String puzzle) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://cswebcat.swansea.ac.uk/message?solution=" + puzzle))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
+
+
 }
 
 
