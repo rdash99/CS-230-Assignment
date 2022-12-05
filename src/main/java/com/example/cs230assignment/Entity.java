6ad351486c52bfbb90edcee5079c9f0adb4ba4e4
Rowan Dash <pokemonmaster9967@gmail.com>
2022-12-05 11:11:51 +0000

    Update Entity.java to change getters

--- src/main/java/com/example/cs230assignment/Entity.java
+++ src/main/java/com/example/cs230assignment/Entity.java
@@ -15,7 +15,7 @@ public class Entity extends DrawShape {
     protected int[] coord;
 
     public Entity(String entityNamePass, int x, int y) {
-        this.coord = new int[] {x, y};
+        this.coord = new int[] { x, y };
         this.entityName = entityNamePass;
     }
 
@@ -23,8 +23,12 @@ public class Entity extends DrawShape {
         return this.entityName;
     }
 
-    public int[] getCoord() {
-        return this.coord;
+    public int getXCoord() {
+        return this.coord[0];
+    }
+
+    public int getYCoord() {
+        return this.coord[1];
     }
 
     @Override
