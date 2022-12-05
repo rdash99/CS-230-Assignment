6aeec593aba60cf88559f282c33109f6a3d1bca0
Rowan Dash <pokemonmaster9967@gmail.com>
2022-12-05 11:11:28 +0000

    Fix errors

--- src/main/java/com/example/cs230assignment/FloorFollowingThief.java
+++ src/main/java/com/example/cs230assignment/FloorFollowingThief.java
@@ -7,74 +7,87 @@ package com.example.cs230assignment;
  * @version 1.0
  */
 class FloorFollowingThief extends NPC {
-    private char allocatedColour;
+  private char allocatedColour;
 
-    public FloorFollowingThief(char allocatedColourPass, int x, int y) {
-        // Allocated a timer of 0.5 seconds for the thief
-        super("Floor Following Thief", 0.5, x, y);
-        this.allocatedColour = allocatedColourPass;
-        super.coordChange = new int[]{x,y};
-    }
+  public FloorFollowingThief(char allocatedColourPass, int x, int y) {
+    // Allocated a timer of 0.5 seconds for the thief
+    super("Floor Following Thief", 0.5, x, y);
+    this.allocatedColour = allocatedColourPass;
+    super.coordChange = new int[] { x, y };
+  }
 
-    private void interact() {
+  private void interact() {
 
-    }
+  }
 
-    // needs to compare to the previous coordChange as it needs to stick to the
-    // edge not to the left
-    // needs to check to the left of the previous coord change
-    private void validMove() {
-      //changes the coordChange to the coord to the left
+  // needs to compare to the previous coordChange as it needs to stick to the
+  // edge not to the left
+  // needs to check to the left of the previous coord change
+  private void validMove() {
+    // changes the coordChange to the coord to the left
 
-      if (super.coordChange[0] == 1 && super.coordChange[1] == 0){
+    if (super.coordChange[0] == 1 && super.coordChange[1] == 0) {
       super.coordChange[0] = 0;
       super.coordChange[1] = 1;
-      }else if(super.coordChange[0] == 0 && super.coordChange[1] == 1){
-        super.coordChange[0] = -1;
-        super.coordChange[1] = 0;
-      }else if(super.coordChange[0] == -1 && super.coordChange[1] == 0){
-        super.coordChange[0] = 0;
-        super.coordChange[1] = -1;
-      }else if(super.coordChange[0] == 0 && super.coordChange[1] == -1){
-        super.coordChange[0] = 1;
-        super.coordChange[1] = 0;
-      }
-      //loops in all directions to check for a valid move
-      for (int i = 0; i < 4; i++){
-        if (i != 4){
-          if ((super.coordChange[0] == 1 && super.coordChange[1] == 0) && super.coordX != super.board.getWidth()){
-            if (super.board.getTile(super.coordX,super.coordY+1).checkColour(this.allocatedColour) == true){
-              i = 5;
-            }else{
-              super.coordChange[0] = 0;
-              super.coordChange[1] = -1;
-            }
-          }else if((super.coordChange[0] == 0 && super.coordChange[1] == 1) && super.coordY != super.board.getHeight()){
-            if (super.board.getTile(super.coordX,super.coordY+1).checkColour(this.allocatedColour) == true){
-              i = 5;
-            }else{
-              super.coordChange[0] = 1;
-              super.coordChange[1] = 0;
-            }
-          }else if((super.coordChange[0] == -1 && super.coordChange[1] == 0) && super.coordX != 0){
-            if (super.board.getTile(super.coordX,super.coordY-1).checkColour(this.allocatedColour) == true){
-              i = 5;
-            }else{
-              super.coordChange[0] = 0;
-              super.coordChange[1] = 1;
-            }
-          }else if((super.coordChange[0] == 0 && super.coordChange[1] == -1) && super.coordY != 0){
-            if (super.board.getTile(super.coordX,super.coordY-1).checkColour(this.allocatedColour) == true){
-              i = 5;
-            }else{
-              super.coordChange[0] = -1;
-              super.coordChange[1] = 0;
-            }
+    } else if (super.coordChange[0] == 0 && super.coordChange[1] == 1) {
+      super.coordChange[0] = -1;
+      super.coordChange[1] = 0;
+    } else if (super.coordChange[0] == -1 && super.coordChange[1] == 0) {
+      super.coordChange[0] = 0;
+      super.coordChange[1] = -1;
+    } else if (super.coordChange[0] == 0 && super.coordChange[1] == -1) {
+      super.coordChange[0] = 1;
+      super.coordChange[1] = 0;
+    }
+    // loops in all directions to check for a valid move
+    for (int i = 0; i < 4; i++) {
+      if (i != 4) {
+        if ((super.coordChange[0] == 1 && super.coordChange[1] == 0)
+            && super.getXCoord() != super.currentBoard.getWidth()) {
+          if (super.currentBoard
+              .getTile(super.getXCoord(), super.getYCoord() + 1)
+              .checkColour(this.allocatedColour) == true) {
+            i = 5;
+          } else {
+            super.coordChange[0] = 0;
+            super.coordChange[1] = -1;
           }
-        }else{
-          super.coordChange[0] = 0;
-          super.coordChange[1] = 0;;
+        } else if ((super.coordChange[0] == 0 && super.coordChange[1] == 1)
+            && super.getYCoord() != super.currentBoard.getHeight()) {
+          if (super.currentBoard
+              .getTile(super.getXCoord(), super.getYCoord() + 1)
+              .checkColour(this.allocatedColour) == true) {
+            i = 5;
+          } else {
+            super.coordChange[0] = 1;
+            super.coordChange[1] = 0;
+          }
+        } else if ((super.coordChange[0] == -1 && super.coordChange[1] == 0)
+            && super.getXCoord() != 0) {
+          if (super.currentBoard
+              .getTile(super.getXCoord(), super.getYCoord() - 1)
+              .checkColour(this.allocatedColour) == true) {
+            i = 5;
+          } else {
+            super.coordChange[0] = 0;
+            super.coordChange[1] = 1;
+          }
+        } else if ((super.coordChange[0] == 0 && super.coordChange[1] == -1)
+            && super.getYCoord() != 0) {
+          if (super.currentBoard
+              .getTile(super.getXCoord(), super.getYCoord() - 1)
+              .checkColour(this.allocatedColour) == true) {
+            i = 5;
+          } else {
+            super.coordChange[0] = -1;
+            super.coordChange[1] = 0;
+          }
         }
+      } else {
+        super.coordChange[0] = 0;
+        super.coordChange[1] = 0;
+        ;
       }
     }
+  }
 }
