e3d42d307ae2bb29bf39d544b7e7dcf6f5851c60
charlesOverton <74202093+CharlesOverton@users.noreply.github.com>
2022-12-06 18:06:42 +0000

    added interact for player
    timer needs fixing

--- src/main/java/com/example/cs230assignment/Player.java
+++ src/main/java/com/example/cs230assignment/Player.java
@@ -19,7 +19,29 @@ public class Player extends Character {
 
     // changed to just interact to overwrite from character
     private void interact(Entity interactedEntity) {
-
+        int x = this.coord[0];
+        int y = this.coord[1];
+        for (int i = 0; i < currentBoard.getTile(x, y)
+                .getEntities().length; i++) {
+            if (currentBoard.getTile(x, y).getEntities()[i]
+                    .getEntityName() == "item") {
+                this.score += ((Item)(this.currentBoard.getTile(x, y).getEntities()[i])).getItemValue();
+            } else if (currentBoard.getTile(x, y).getEntities()[i]
+                    .getEntityName() == "key") {
+                Key key = (Key) currentBoard.getTile(x, y).getEntities()[i];
+                key.openGate();
+                currentBoard.getTile(x, y).removeEntity(
+                        currentBoard.getTile(x, y).getEntities()[i]);
+            } else if (currentBoard.getTile(x, y).getEntities()[i]
+                    .getEntityName() == "clock") {
+                Clock clock = (Clock) currentBoard.getTile(x, y)
+                        .getEntities()[i];
+                timer.addClock(clock);
+            } else if (currentBoard.getTile(x, y).getEntities()[i]
+                    .getEntityName() == "player") {
+                currentBoard.getPlayer().die();
+            }
+        }
     }
 
     // changed to take the item in as different items will add different scores
