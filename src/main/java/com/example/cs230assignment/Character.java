b10a74640b85746a745ee8279747157d768e3ecc
charlesOverton <74202093+CharlesOverton@users.noreply.github.com>
2022-12-06 15:04:23 +0000

    changed board name back

--- src/main/java/com/example/cs230assignment/Character.java
+++ src/main/java/com/example/cs230assignment/Character.java
@@ -6,7 +6,7 @@ public class Character extends Entity {
     }
 
     // moved up from NPC as the player will also need Board
-    protected Board board;
+    protected Board currentBoard;
 
     protected void move() {
 
