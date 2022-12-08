493b6b5b05205aab5c1fb25ab6306810ec89dd97
Frasukai <69215668+Frasukai@users.noreply.github.com>
2022-12-08 16:37:51 +0000

    javadoc

--- src/main/java/com/example/cs230assignment/MainMenu.java
+++ src/main/java/com/example/cs230assignment/MainMenu.java
@@ -4,11 +4,9 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -21,6 +19,15 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
+
+/**
+ * The main menu class. It is used to display the main menu which contains
+ * the scores, the message of the day and allows the player to enter their name
+ * and start the game.
+ * @author Fraser Clough
+ * @version 1.0
+ */
+
 
 public class MainMenu extends Stage {
 
@@ -37,6 +44,11 @@ public class MainMenu extends Stage {
     private VBox motd = new VBox();
     private HBox hbox = new HBox();
 
+    /**
+     * Creates a new Main Menu when this class is instantiated.
+     * @throws IOException if the http request fails.
+     * @throws InterruptedException if the http request is interrupted.
+     */
     public MainMenu() throws IOException, InterruptedException {
         titleText.setFont(Font.font("Arial", FontWeight.BOLD, 20));
         titleText.setFill(Color.GRAY);
@@ -49,7 +61,7 @@ public class MainMenu extends Stage {
         motdTitle.setFill(Color.GRAY);
         motdTitle.setWrappingWidth(100);
 
-        motdText = new Text(getsolvedMOTD(solvePuzzle()));
+        motdText = new Text(getSolvedMOTD(solvePuzzle()));
         motdText.setFont(Font.font("Arial", 12));
         motdText.setFill(Color.GRAY);
         motdText.setWrappingWidth(100);
@@ -82,15 +94,15 @@ public class MainMenu extends Stage {
         this.setTitle("The game menu");
         this.show();
 
-        // end action
+        // When the button is clicked, the game is once a name has been provided.
         launchGameBtn.setOnAction(e -> {
             if ((nameField.getText() != null && !nameField.getText().isEmpty())) {
                 new GameGUI(nameField.getText());
                 this.close();
             } else if (errorText.getText().isEmpty()) {
-               errorText.setText("Please enter your name");
-               errorText.setFill(Color.RED);
-               middleButtons.getChildren().add(errorText);
+                errorText.setText("Please enter your name");
+                errorText.setFill(Color.RED);
+                middleButtons.getChildren().add(errorText);
 
             }
 
@@ -98,6 +110,11 @@ public class MainMenu extends Stage {
 
     }
 
+    /**
+     * Adds the high scores to the high score list.
+     * @param name
+     * @param score
+     */
     public void addHighScore(String name, int score) {
         Text scoreText = new Text(name + ": " + score);
         scoreText.setFont(Font.font("Arial", 12));
@@ -106,6 +123,12 @@ public class MainMenu extends Stage {
         highScoreList.getChildren().add(scoreText);
     }
 
+    /**
+     * Gets the message of the day puzzle from the server.
+     * @return the message of the day puzzle.
+     * @throws IOException if the http request fails.
+     * @throws InterruptedException if the http request is interrupted.
+     */
     private String getMOTDPuzzle() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -116,15 +139,23 @@ public class MainMenu extends Stage {
         return response.body();
     }
 
+    /**
+     * Solves the message of the day puzzle.
+     * @return the solution to the message of the day puzzle.
+     * @throws IOException if the http request fails.
+     * @throws InterruptedException if the http request is interrupted.
+     */
     private String solvePuzzle() throws IOException, InterruptedException {
         String puzzle = getMOTDPuzzle();
         ArrayList<Integer> charNums = new ArrayList<Integer>();
 
+        // Converts the puzzle into an array of integers.
         String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         for (int i = 0; i < puzzle.length(); i++) {
             charNums.add((alphabet.indexOf(puzzle.charAt(i))));
         }
 
+        // Computes the solution using integer arithmetic.
         for (int i = 0; i < charNums.size(); i++) {
             if (i % 2 != 0) {
                 charNums.set(i, charNums.get(i) + (i + 1));
@@ -148,26 +179,35 @@ public class MainMenu extends Stage {
             }
         }
 
+        // Converts the array of integers back into an array of Characters.
         ArrayList<java.lang.Character> charRes = new ArrayList<java.lang.Character>();
         for (int i = 0; i < charNums.size(); i++) {
             charRes.add(alphabet.charAt(charNums.get(i)));
         }
 
+        // Converts the array of Characters into a String.
         StringBuilder builder = new StringBuilder(charRes.size());
         for (java.lang.Character cha : charRes) {
             builder.append(cha);
         }
+        // Adds the additional prefix to the solved puzzle.
         String result = builder.toString().concat("CS-230");
         result = result.chars().count() + result;
 
         return result;
     }
 
-    private String getsolvedMOTD(String puzzle)
-            throws IOException, InterruptedException {
+    /**
+     * Gets the message of the day from the server using a computed solution.
+     * @param solution the solved message of the day puzzle.
+     * @return the message of the day.
+     * @throws IOException if the http request fails.
+     * @throws InterruptedException if the http request is interrupted.
+     */
+    private String getSolvedMOTD(String solution) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(
-                "http://cswebcat.swansea.ac.uk/message?solution=" + puzzle))
+                        "http://cswebcat.swansea.ac.uk/message?solution=" + solution))
                 .build();
         HttpResponse<String> response = client.send(request,
                 HttpResponse.BodyHandlers.ofString());
