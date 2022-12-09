2bfcb18bb605115f18ca9e388c38464b91a4d1ed
belairemaxwell <professowell217@gmail.com>
2022-12-09 21:11:57 +0000

    Explode bomb has been written

--- src/main/java/com/example/cs230assignment/Bomb.java
+++ src/main/java/com/example/cs230assignment/Bomb.java
@@ -1,8 +1,11 @@
 package com.example.cs230assignment;
 
+import java.util.ArrayList;
+
 public class Bomb extends Entity {
     private int xCoord;
     private int yCoord;
+    protected Board currentBoard;
 
     public Bomb(int x, int y){
         super("Bomb", x, y);
@@ -10,9 +13,15 @@ public class Bomb extends Entity {
         this.yCoord = y;
     }
 
-    //Method removes all loot, clocks and levers and bombs
+
     /**
      * starts the proccess of detonating the bomb
+     */
+
+    /**
+     * Needs to access all the tiles that sit on the x and y axis of the bomb.
+     * Then needs to decide whether there is an entity on these tiles.
+     * If there is an entity on the tile then the entity needs to be deleted
      */
     public void explodeBomb(){
         Timer bombClock = new Timer(3);
@@ -20,8 +29,31 @@ public class Bomb extends Entity {
         while (bombClock.getLevelTime() > 0) {
             bombClock.decrementTime();
         }
-
-        if ()
-
+        /**
+         * This part of the method is going to check the x coord
+         * for items
+         */
+        for (int i = 0; i < currentBoard.getTiles().length; i++) {
+            if (currentBoard.getTile(xCoord, i).getEntity() != null) {
+                currentBoard.getTile(xCoord, i).removeEntity();
+            } else if (currentBoard.getTile(xCoord, i).getEntity().getEntityName() == "bomb") {
+                explodeBomb();
+            } else {
+                i++;
+            }
+        }
+        /**
+         * This part of the method is going to check the y coord
+         * for items
+         */
+        for (int i = 0; i < currentBoard.getTiles().length; i++) {
+            if (currentBoard.getTile(i, yCoord).getEntity() != null) {
+                currentBoard.getTile(i, yCoord).removeEntity();
+            } else if (currentBoard.getTile(i, yCoord).getEntity().getEntityName() != "bomb") {
+                explodeBomb();
+            } else {
+                i++;
+            }
+        }
     }
 }
