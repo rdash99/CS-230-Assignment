f06d10e96437870f03a55a175f0e5ba00b5ff81f
charlesOverton <74202093+CharlesOverton@users.noreply.github.com>
2022-12-11 22:49:27 +0000

    FFT MOVEMENT
    allowed the fft to move in the correct way

--- src/main/java/com/example/cs230assignment/FloorFollowingThief.java
+++ src/main/java/com/example/cs230assignment/FloorFollowingThief.java
@@ -45,6 +45,8 @@ class FloorFollowingThief extends NPC {
   public void move(GraphicsContext gc) {
     Boolean x = validMove();
     if (x) {
+      System.out.println(this.coordChange[0]);
+      System.out.println(this.coordChange[1]);
       super.interact(this.coord[0] + this.coordChange[0],
           this.coord[1] + this.coordChange[1]);
       Character.currentBoard.getTile(this.coord[0], this.coord[1])
@@ -55,6 +57,9 @@ class FloorFollowingThief extends NPC {
       this.coord[1] = this.coord[1] + this.coordChange[1];
       System.out.println("Coords: " + this.coord[0] + "" + this.coord[1]);
       Character.currentBoard.getTimer().boardUpdate(gc, Character.currentBoard);
+    }else {
+      System.out.println(this.coordChange[0]);
+      System.out.println(this.coordChange[1]);
     }
   }
 
@@ -85,212 +90,127 @@ class FloorFollowingThief extends NPC {
       // checks to see if it has checked all directions else sets direction to
       // stay on square
       if (i != 4) {
-        // checks to see if there is a tile to the right side of the board of
-        // the thief
-        if ((super.coordChange[0] == 1 && super.coordChange[1] == 0)
-            && super.getXCoord() != super.getCurrentBoard().getWidth()) {
-          // checks to see if the tile is the correct colour
-          if (super.getCurrentBoard()
-              .getTile(super.getXCoord(), super.getYCoord() + 1)
-              .checkColour(this.allocatedColour) == true) {
-            // checks to see if there is an entity on the tile
-            if (!(super.getCurrentBoard()
-                .getTile(super.getXCoord(), super.getYCoord() + 1)
-                .getEntity() != null)) {
-              // checks to see if the entity on the tile is one that will block
-              // movement
-              // if not the valid move has been found
-              // if it is check another direction
-              switch (Character.currentBoard
-                  .getTile(super.getXCoord(), super.getYCoord() + 1).getEntity()
-                  .getEntityName()) {
-              case ("Gate"):
-                if (!((Gate) Character.currentBoard
-                    .getTile(super.getXCoord(), super.getYCoord() + 1)
-                    .getEntity()).getGateOpen()) {
-                  i = 5;
-                } else {
-                  super.coordChange[0] = -1;
-                  super.coordChange[1] = 0;
+        if (super.coordChange[0] == 1 && super.coord[0] != Character.currentBoard.getWidth()-1){
+          if (Character.currentBoard.getTile(coord[1], coord[0] + 1).checkColour(this.allocatedColour)){
+            if (Character.currentBoard.getTile(coord[0] + 1, coord[1]).getEntity() != null){
+              if (Character.currentBoard.getTile(coord[0] + 1, coord[1]).getEntity() instanceof Gate){
+                if (((Gate)Character.currentBoard.getTile(coord[0] + 1, coord[1]).getEntity()).getGateOpen()){
+                  return true;
+                }else {
+                  coordChange[0] = 0;
+                  coordChange[1] = -1; 
                 }
-                break;
-              case ("FloorFollowingThief"):
-              case ("FlyingAssassin"):
-              case ("SmartThief"):
-                super.coordChange[0] = 0;
-                super.coordChange[1] = -1;
-                break;
-              case ("Player"):
-                return false;
-              // breaks out of the loop as the correct direction has been found
-              default:
-                i = 5;
               }
-              // breaks out of the loop as the valid move has been found
-            } else {
-              i = 5;
+              if(Character.currentBoard.getTile(coord[0] + 1, coord[1]).getEntity() instanceof FloorFollowingThief 
+              ||Character.currentBoard.getTile(coord[0] + 1, coord[1]).getEntity() instanceof Player 
+              ||Character.currentBoard.getTile(coord[0] + 1, coord[1]).getEntity() instanceof FlyingAssassin 
+              ||Character.currentBoard.getTile(coord[0] + 1, coord[1]).getEntity() instanceof SmartThief){
+                return false;
+              }else{
+                return true;
+              }
+            }else {
+              return true;
             }
-            // incorrect colour check next direction
-          } else {
-            super.coordChange[0] = 0;
-            super.coordChange[1] = -1;
+          }else {
+            coordChange[0] = 0;
+            coordChange[1] = -1; 
           }
-          // checks to see if there is a valid move up from the
-        } else if ((super.coordChange[0] == 0 && super.coordChange[1] == 1)
-            && super.getYCoord() != super.getCurrentBoard().getHeight()) {
-          // checks to see if the tile is the correct colour
-          if (super.getCurrentBoard()
-              .getTile(super.getXCoord(), super.getYCoord() + 1)
-              .checkColour(this.allocatedColour) == true) {
-            // checks to see if there is an entity on the tile
-            if (!(super.getCurrentBoard()
-                .getTile(super.getXCoord(), super.getYCoord() + 1)
-                .getEntity() != null)) {
-              // checks to see if the entity on the tile blocks movement
-              // if not valid move has been found
-              // else breaks out of the loop as the correct direction has been
-              // found
-              switch (Character.currentBoard
-                  .getTile(super.getXCoord(), super.getYCoord() + 1).getEntity()
-                  .getEntityName()) {
-              case ("Gate"):
-                if (!((Gate) Character.currentBoard
-                    .getTile(super.getXCoord(), super.getYCoord() + 1)
-                    .getEntity()).getGateOpen()) {
-                  i = 5;
-                } else {
-                  super.coordChange[0] = -1;
-                  super.coordChange[1] = 0;
+        }else if(super.coordChange[0] == 1 && super.coord[0] == Character.currentBoard.getWidth()-1) {
+          coordChange[0] = 0;
+          coordChange[1] = -1; 
+        }else if (super.coordChange[0] == -1 && super.coord[0] != 0){
+          if (Character.currentBoard.getTile(coord[1], coord[0] - 1).checkColour(this.allocatedColour)){
+            if (Character.currentBoard.getTile(coord[0] - 1, coord[1]).getEntity() != null){
+              if (Character.currentBoard.getTile(coord[0] - 1, coord[1]).getEntity() instanceof Gate){
+                if (((Gate)Character.currentBoard.getTile(coord[0] - 1, coord[1]).getEntity()).getGateOpen()){
+                  return true;
+                }else {
+                  coordChange[0] = 0;
+                  coordChange[1] = 1; 
                 }
-                break;
-              case ("FloorFollowingThief"):
-              case ("FlyingAssassin"):
-              case ("SmartThief"):
-                super.coordChange[0] = 1;
-                super.coordChange[1] = 0;
-                break;
-              case ("Player"):
-                return false;
-              // breaks out of the loop as the correct direction has been found
-              default:
-                i = 5;
               }
-              // valid move breaks out of loop as correct direction has been
-              // found
-            } else {
-              i = 5;
+              if(Character.currentBoard.getTile(coord[0] - 1, coord[1]).getEntity() instanceof FloorFollowingThief 
+              ||Character.currentBoard.getTile(coord[0] - 1, coord[1]).getEntity() instanceof Player 
+              ||Character.currentBoard.getTile(coord[0] - 1, coord[1]).getEntity() instanceof FlyingAssassin 
+              ||Character.currentBoard.getTile(coord[0] - 1, coord[1]).getEntity() instanceof SmartThief){
+                return false;
+              }else{
+                return true;
+              }
+            }else {
+              return true;
             }
-            // incorrect colour invalid move checks another direction
-          } else {
-            super.coordChange[0] = 1;
-            super.coordChange[1] = 0;
+          }else {
+            coordChange[0] = 0;
+            coordChange[1] = 1; 
           }
-          // checks left of the thief on the board for a valid move
-        } else if ((super.coordChange[0] == -1 && super.coordChange[1] == 0)
-            && super.getXCoord() != 0) {
-          // checks to see if the tile is the correct colour
-          if (super.getCurrentBoard()
-              .getTile(super.getXCoord(), super.getYCoord() + 1)
-              .checkColour(this.allocatedColour) == true) {
-            // checks to see if there is an entity on the tile
-            if (!(super.getCurrentBoard()
-                .getTile(super.getXCoord(), super.getYCoord() + 1)
-                .getEntity() != null)) {
-              // checks to see if there is an entity on the tile that blocks
-              // movement
-              // if it is a movement blocking entity check another direction
-              // else break out of the loop as it is a valid move
-              switch (Character.currentBoard
-                  .getTile(super.getXCoord(), super.getYCoord() + 1).getEntity()
-                  .getEntityName()) {
-              case ("Gate"):
-                if (!((Gate) Character.currentBoard
-                    .getTile(super.getXCoord(), super.getYCoord() + 1)
-                    .getEntity()).getGateOpen()) {
-                  i = 5;
-                } else {
-                  super.coordChange[0] = -1;
-                  super.coordChange[1] = 0;
+        }else if(super.coordChange[0] == -1 && super.coord[0] == 0) {
+          coordChange[0] = 0;
+          coordChange[1] = 1; 
+        }else if (super.coordChange[1] == 1 && super.coord[1] != Character.currentBoard.getHeight()-1){
+          if (Character.currentBoard.getTile(coord[1] + 1, coord[0]).checkColour(this.allocatedColour)){
+            if (Character.currentBoard.getTile(coord[0], coord[1] + 1).getEntity() != null){
+              if (Character.currentBoard.getTile(coord[0], coord[1] + 1).getEntity() instanceof Gate){
+                if (((Gate)Character.currentBoard.getTile(coord[0], coord[1] + 1).getEntity()).getGateOpen()){
+                  return true;
+                }else {
+                  coordChange[0] = 1;
+                  coordChange[1] = 0; 
                 }
-                break;
-              case ("FloorFollowingThief"):
-              case ("FlyingAssassin"):
-              case ("SmartThief"):
-                super.coordChange[0] = 0;
-                super.coordChange[1] = 1;
-                break;
-              case ("Player"):
-                return false;
-              // breaks out of the loop as the correct direction has been found
-              default:
-                i = 5;
               }
-              // valid move break out of loop as correct direction has been
-              // found
-            } else {
-              i = 5;
+              if(Character.currentBoard.getTile(coord[0], coord[1] + 1).getEntity() instanceof FloorFollowingThief 
+              ||Character.currentBoard.getTile(coord[0], coord[1] + 1).getEntity() instanceof Player 
+              ||Character.currentBoard.getTile(coord[0], coord[1] + 1).getEntity() instanceof FlyingAssassin 
+              ||Character.currentBoard.getTile(coord[0], coord[1] + 1).getEntity() instanceof SmartThief){
+                return false;
+              }else{
+                return true;
+              }
+            }else {
+              return true;
             }
-            // incorrect colour invalid move check a new direction
-          } else {
-            super.coordChange[0] = 0;
-            super.coordChange[1] = 1;
+          }else {
+            coordChange[0] = 1;
+            coordChange[1] = 0; 
           }
-          // checks below the thief on the board for a valid move
-        } else if ((super.coordChange[0] == 0 && super.coordChange[1] == -1)
-            && super.getYCoord() != 0) {
-          // checks to see if the tile is the correct colour
-          if (super.getCurrentBoard()
-              .getTile(super.getXCoord(), super.getYCoord() + 1)
-              .checkColour(this.allocatedColour) == true) {
-            // checks to see if the tile contains an entity
-            if (!(super.getCurrentBoard()
-                .getTile(super.getXCoord(), super.getYCoord() + 1)
-                .getEntity() != null)) {
-              // checks to see if the entity on the tile blocks movement
-              // if it is check a different direction
-              // if it isnt then its a valid move
-              switch (Character.currentBoard
-                  .getTile(super.getXCoord(), super.getYCoord() + 1).getEntity()
-                  .getEntityName()) {
-              case ("Gate"):
-                if (!((Gate) Character.currentBoard
-                    .getTile(super.getXCoord(), super.getYCoord() + 1)
-                    .getEntity()).getGateOpen()) {
-                  i = 5;
-                } else {
-                  super.coordChange[0] = -1;
-                  super.coordChange[1] = 0;
+        }else if(super.coordChange[1] == 1 && super.coord[1] == Character.currentBoard.getHeight()-1) {
+          coordChange[0] = 1;
+          coordChange[1] = 0; 
+        }else if (super.coordChange[1] == -1 && super.coord[1] != 0){
+          if (Character.currentBoard.getTile(coord[1] - 1, coord[0]).checkColour(this.allocatedColour)){
+            if (Character.currentBoard.getTile(coord[0], coord[1] - 1).getEntity() != null){
+              if (Character.currentBoard.getTile(coord[0], coord[1] - 1).getEntity() instanceof Gate){
+                if (((Gate)Character.currentBoard.getTile(coord[0], coord[1] - 1).getEntity()).getGateOpen()){
+                  return true;
+                }else {
+                  coordChange[0] = -1;
+                  coordChange[1] = 0; 
                 }
-                break;
-              case ("FloorFollowingThief"):
-              case ("FlyingAssassin"):
-              case ("SmartThief"):
-                super.coordChange[0] = -1;
-                super.coordChange[1] = 0;
-                break;
-              case ("Player"):
-                return false;
-              // breaks out of the loop as the correct direction has been found
-              default:
-                i = 5;
               }
-              // valid move breaks out of loop as the correct direction has been
-              // found
-            } else {
-              i = 5;
+              if(Character.currentBoard.getTile(coord[0], coord[1] - 1).getEntity() instanceof FloorFollowingThief 
+              ||Character.currentBoard.getTile(coord[0], coord[1] - 1).getEntity() instanceof Player 
+              ||Character.currentBoard.getTile(coord[0], coord[1] - 1).getEntity() instanceof FlyingAssassin 
+              ||Character.currentBoard.getTile(coord[0], coord[1] - 1).getEntity() instanceof SmartThief){
+                return false;
+              }else{
+                return true;
+              }
+            }else {
+              return true;
             }
-            // invalid move as incorrect colour
-          } else {
-            super.coordChange[0] = -1;
-            super.coordChange[1] = 0;
+          }else {
+            coordChange[0] = -1;
+            coordChange[1] = 0; 
           }
+        }else if(super.coordChange[1] == -1 && super.coord[1] == 0) {
+          coordChange[0] = -1;
+          coordChange[1] = 0; 
         }
-        // sets the movement to make the thief stand still as there is no valid
-        // move
       } else {
         return false;
       }
     }
-    return true;
+    return false;
   }
 }
